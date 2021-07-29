a8a549a1024da3c6dc4f7af103e508ea1b0cb5e8
GriffinBonner <griffinqbonner@gmail.com>
2021-07-29 11:04:37 -0400

    dma interrupt request handler

--- src/ai_hover.c
+++ src/ai_hover.c
@@ -18,13 +18,14 @@
 #include "log.h"
 #include "param.h"
 
+/* expand for additional network outputs */
 #define BUFFERSIZE 1
 
 uint8_t aideckRxBuffer[BUFFERSIZE];
 volatile uint8_t dma_flag = 0;
 uint8_t obs_likelihood=0;
 
-/* crazyflie states */
+/* application states */
 typedef enum {
   idle,
   flying,
@@ -32,7 +33,7 @@ typedef enum {
   stopping
 } State;
 
-/* pack setpoint sent to commander */
+/* pack hover setpoint sent to commander */
 static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float z, float yawrate)
 {
   setpoint->mode.z = modeAbs;
@@ -49,32 +50,38 @@ static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float z, 
   setpoint->velocity_body = true;
 }
 
+static State app_state = idle;
+
+
 void appMain()
 {
   DEBUG_PRINT("ai-hover application started... \n");
   USART_DMA_Start(115200, aideckRxBuffer, BUFFERSIZE);
+  
+  static setpoint_t setpoint;
+  app_state = flying;
 
-  while(1) {
+  while(1)
+  {
     vTaskDelay(M2T(100));
     if (dma_flag == 1)
-      {
-	dma_flag = 0;  // clear the flag
-	
-	// print the inference likelihood of obstacle to debug console
-	DEBUG_PRINT("obstacle likelihood: %d\n", aideckRxBuffer[0]);
-	obs_likelihood = aideckRxBuffer[0]; // save likehihood
-	memset(aideckRxBuffer, 0, BUFFERSIZE);  // clear the dma buffer
-      }
+    {
+	    dma_flag = 0; // clear the flag
+	    DEBUG_PRINT("obstacle likelihood: %d\n", aideckRxBuffer[0]); // display likelihood
+	    obs_likelihood = aideckRxBuffer[0]; // save likelihood
+	    memset(aideckRxBuffer, 0, BUFFERSIZE);  // clear the dma buffer
+    }
   }
 }
 
-
+/* uart dma interrupt request handler */
 void __attribute__((used)) DMA1_Stream1_IRQHandler(void)
 {
   DMA_ClearFlag(DMA1_Stream1, UART3_RX_DMA_ALL_FLAGS);
   dma_flag = 1;
 }
 
+/* add obstacle likelihood as log variable */
 LOG_GROUP_START(log_test)
 LOG_ADD(LOG_UINT8, test_variable_x, &obs_likehihood)
 LOG_GROUP_STOP(log_test)
